de39670e773c05369563b40e402c13b7aca7af18
atom <reallygud@protonmail.com>
2025-10-17 14:53:52 +0200

    Revert "flyways' V1 now uses self generated migration"
    This reverts commit ff58b2ae0768fe9a21f11a34a02f44d515bbdeae.

--- src/main/resources/db/migration/V1__rename_md5_to_hash.sql
+++ src/main/resources/db/migration/V1__rename_md5_to_hash.sql
@@ -1,11 +1,1 @@
-ALTER TABLE file_upload
-    ADD hash VARCHAR(255);
-
-ALTER TABLE file_upload
-    ALTER COLUMN hash SET NOT NULL;
-
-ALTER TABLE file_upload
-    ADD CONSTRAINT uc_fileupload_hash UNIQUE (hash);
-
-ALTER TABLE file_upload
-    DROP COLUMN md5;
+ALTER TABLE file_upload RENAME COLUMN md5 TO hash;
