ff58b2ae0768fe9a21f11a34a02f44d515bbdeae
atom <reallygud@protonmail.com>
2025-10-17 14:51:35 +0200

    flyways' V1 now uses self generated migration

--- src/main/resources/db/migration/V1__rename_md5_to_hash.sql
+++ src/main/resources/db/migration/V1__rename_md5_to_hash.sql
@@ -1,1 +1,11 @@
-ALTER TABLE file_upload RENAME COLUMN md5 TO hash;
+ALTER TABLE file_upload
+    ADD hash VARCHAR(255);
+
+ALTER TABLE file_upload
+    ALTER COLUMN hash SET NOT NULL;
+
+ALTER TABLE file_upload
+    ADD CONSTRAINT uc_fileupload_hash UNIQUE (hash);
+
+ALTER TABLE file_upload
+    DROP COLUMN md5;
